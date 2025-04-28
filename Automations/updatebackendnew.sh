8883f2be5ecc54c5c6f10908647409ca3cd836d5
bindu <bindupriya2508@gmail.com>
2025-04-28 19:15:06 +0530

    Update updatebackendnew.sh

--- Automations/updatebackendnew.sh
+++ Automations/updatebackendnew.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the Instance ID and path to the .env file
-INSTANCE_ID="i-0a4ba6c3a5b23dc99"
+INSTANCE_ID="i-030da7d31a1dbbffc"
 
 # Retrieve the public IP address of the specified EC2 instance
 ipv4_address=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
